f1303a841dab35d747e8b30b6ccea60b86820e61
Bhuvan <bhuvan13087@gmail.com>
2026-03-27 13:18:53 +0000

    Try fixing this error: `Build Error: Ecmascript file had an error. Error

--- src/app/(dashboard)/rounds/page.tsx
+++ src/app/(dashboard)/rounds/page.tsx
@@ -40,6 +40,7 @@ import {
   AlertDialogCancel,
   AlertDialogContent,
   AlertDialogDescription,
+  AlertDialogFooter,
   AlertDialogHeader,
   AlertDialogTitle,
 } from "@/components/ui/alert-dialog"
